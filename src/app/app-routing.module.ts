8a420986fcd68a3d4aa285c069c4b7fc98c7cea1
TranThiLinhChi <70497465+TranThiLinhChi@users.noreply.github.com>
2020-10-01 23:27:57 +0700

    BanDongHo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {ListItemComponent} from './product/list-item/list-item.component';
+import {NotFoundComponent} from './shared/not-found/not-found.component';
+
+const routes: Routes = [
+    { path: '',component: HomeComponent },
+    {path: 'list-item', component: ListItemComponent},
+     //{path: '', loadChildren:() => import('./home/home.module').then(m => m.HomeModule)  },
+     {path: 'search', loadChildren:() => import('./search/search.module').then(m => m.SearchModule)  },
+     {path: 'product', loadChildren:() => import('./product/product.module').then(m => m.ProductModule)  },
+     {path: '**', component: NotFoundComponent},
+];
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
