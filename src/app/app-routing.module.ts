96fab547e23d0f0d5b01529148a83ef9c3442037
TranThiLinhChi <70497465+TranThiLinhChi@users.noreply.github.com>
2020-10-08 22:42:00 +0700

    Revert "update"
    This reverts commit 6815f0babecb0f7d88dfc75726f6f83763a5ad20.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {ListItemComponent} from './product/list-item/list-item.component';
-import {DetailComponent} from './product/detail/detail.component';
-//import {CartComponent} from './product/Cart/cart.component';
 import {NotFoundComponent} from './shared/not-found/not-found.component';
 
 const routes: Routes = [
     { path: '',component: HomeComponent },
     {path: 'list-item', component: ListItemComponent},
-    {path: 'detail', component: DetailComponent},
-    //{path: 'cart', component: CartComponent},
-     {path: '', loadChildren:() => import('./home/home.module').then(m => m.HomeModule)  },
+     //{path: '', loadChildren:() => import('./home/home.module').then(m => m.HomeModule)  },
      {path: 'search', loadChildren:() => import('./search/search.module').then(m => m.SearchModule)  },
      {path: 'product', loadChildren:() => import('./product/product.module').then(m => m.ProductModule)  },
      {path: '**', component: NotFoundComponent},
